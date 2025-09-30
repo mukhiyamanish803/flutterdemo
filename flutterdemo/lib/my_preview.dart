eff4b628acc83decbae9c2ce88bc3d240ef4cfe7
Manish Mukhiya <mukhiyamanish803@gmail.com>
2025-09-30 09:46:14 +0000

    commit

---
+++ flutterdemo/lib/my_preview.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/widget_previews.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:widgetbook/widgetbook.dart'; // or your preview package
+// import 'main.dart'; // Import your app
+
+// class MyPreview extends PreviewProvider {
+//   @override
+//   List<Preview> get previews {
+//     return [
+//       Preview(
+//         frame: Frames.ipadPro12,
+//         child: const MainApp(),
+//       ),
+//       Preview(
+//         frame: Frames.iphoneX,
+//         child: const MainApp(),
+//       ),
+//     ];
+//   }
+// }
